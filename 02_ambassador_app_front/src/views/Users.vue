8a1c2a46f3d11f8b1ba117053bd682a70c990a40
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-25 17:52:13 -0300

    Refactoring users table

--- 02_ambassador_app_front/src/views/Users.vue
+++ 02_ambassador_app_front/src/views/Users.vue
@@ -1,26 +1,5 @@
 <template>
   <div>
-  <v-table theme="dark">
-    <thead>
-      <tr>
-        <th class="text-left">
-          Name
-        </th>
-        <th class="text-left">
-          Calories
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-      </tr>
-    </tbody>
-  </v-table>
     <v-table>
       <template v-slot:default>
         <thead>
