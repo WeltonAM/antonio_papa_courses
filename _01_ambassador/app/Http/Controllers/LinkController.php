48d6b6b47686474c69aec22f522d1f66c64c8660
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-25 10:02:59 -0300

    Link store endpoint

--- _01_ambassador/app/Http/Controllers/LinkController.php
+++ _01_ambassador/app/Http/Controllers/LinkController.php
@@ -3,13 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\LinkProduct;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Str;
+use Symfony\Component\HttpFoundation\Response;
 
 class LinkController extends Controller
 {
     public function index(User $user)
     {
         return Link::where('user_id', $user->id)->get();
+    }
+
+    public function store(Request $request, User $user)
+    {
+        $link = Link::create([
+            'user_id' => $request->user()->id,
+            'code' => Str::random(6),
+        ]);
+
+        foreach($request->input('product_ids', []) as $product_id) {
+            LinkProduct::create([
+                'link_id' => $link->id,
+                'product_id' => $product_id,
+            ]);
+        }
+
+        return response($link, Response::HTTP_CREATED);
     }
 }
--- _01_ambassador/routes/api.php
+++ _01_ambassador/routes/api.php
@@ -47,5 +47,7 @@ Route::prefix('ambassador')->group(function () {
     Route::middleware(['auth:sanctum', ScopeAmbassadorMiddleware::class])->group(function () {
         Route::get('stats', [StatsController::class, 'index'])->name('stats');
         Route::get('rankings', [StatsController::class, 'rankings'])->name('rankings');
+
+        Route::post('links', [LinkController::class, 'store'])->name('links.store');
     });
 });
